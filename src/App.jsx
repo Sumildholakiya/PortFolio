c5560fe7848db8693523ba4878a48db69aff785a
Sumil Dholakiya <149218667+Sumildholakiya@users.noreply.github.com>
2025-05-23 11:27:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,6 @@ const App = () => {
     <section id="Contact">
       <Contact/>
     </section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
     {/* <section id="Contact">Contact</section> */}
   </div>;
 };
